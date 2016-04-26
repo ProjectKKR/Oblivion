ea533df2e27adc02787224af126560f98da8867e
rhs0266 <dkflfkdeh@naver.com>
2016-04-26 13:41:12 +0900

    add umbrella, door switch, light switch

---
+++ Assets/Scripts/DoorSwitch/DoorSwitchController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSwitchController : GameItems {
+	public GameObject switchAxis;
+	private float angleOn = (float)(-25);
+	private float angleOff = (float)(+25);
+	private int state = 0;
+
+	void Start () {
+		distanceThreshold = 2.0f;
+		interactable = true;
+	}
+
+	protected override void Interaction () {
+		Debug.Log ("CLICK!");
+		state = 1 - state;
+		if (state==1) switchAxis.transform.Rotate (new Vector3 (0, 0, angleOn));
+		if (state==0) switchAxis.transform.Rotate (new Vector3 (0, 0, angleOff));
+	}
+
+	protected override void PostProcess () {
+		;
+	}
+}
---
+++ Assets/Scripts/LightSwitch/LightSwitchController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitchController : GameItems {
+
+	public GameObject switchAxis;
+
+	private float angleOn = (float)(-10);
+	private float angleOff = (float)(+10);
+	private int state = 0;
+
+	void Start () {
+		distanceThreshold = 2.0f;
+		interactable = true;
+	}
+
+	protected override void Interaction () {
+		Debug.Log ("CLICK!");
+		state = 1 - state;
+		if (state==1) switchAxis.transform.Rotate (new Vector3 (0, 0, angleOn));
+		if (state==0) switchAxis.transform.Rotate (new Vector3 (0, 0, angleOff));
+	}
+
+	protected override void PostProcess () {
+		;
+	}
+}
