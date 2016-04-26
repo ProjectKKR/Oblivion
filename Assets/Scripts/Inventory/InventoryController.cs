5c4bca120dee72499d02516416299dcf781cb250
dn0530 <eodmsabc@gmail.com>
2016-04-26 21:41:03 +0900

    Inventory Screen

---
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+
+public class InventoryController : MonoBehaviour, IPointerDownHandler {
+	private RawImage invButton;
+	private RawImage invPoint;
+	public GameObject invPlane;
+	private int scale = 6;
+	private int time = 0;
+	private bool opened = false;
+
+	void Start () {
+		invButton = GetComponent<RawImage> ();
+		invPoint = transform.GetChild (0).GetComponent<RawImage> ();
+	}
+
+	void Update () {
+		if (opened && time <= 20) {
+			transform.localPosition = new Vector3 (0, time*scale - 205, 0);
+			invPoint.transform.eulerAngles = new Vector3 (0, 0, time / 20.0f * 180);
+			invPlane.transform.localPosition = new Vector3 (0, time*scale - 285, 0);
+			time++;
+		} else if ((!opened) && time >= 0) {
+			transform.localPosition = new Vector3 (0, time*scale - 205, 0);
+			invPoint.transform.eulerAngles = new Vector3 (0, 0, time / 20.0f * 180);
+			invPlane.transform.localPosition = new Vector3 (0, time*scale - 285, 0);
+			time--;
+		}
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped){
+		opened = !opened;
+	}
+}
