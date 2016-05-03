7ff1dea78d3c2914c76034ec8e9166179fa27dfa
rhs0266 <dkflfkdeh@naver.com>
2016-05-03 19:59:23 +0900

    Key Collect

---
+++ Assets/Scripts/KeyTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyTrigger: GameItems{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	protected override bool EquippedItemCheck(GameItems equipped) {
+		return true;
+	}
+
+	protected override void Interaction () {
+		// TODO
+	}
+	protected override void PostProcess () {
+		;
+	}
+}
