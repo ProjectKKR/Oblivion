5b59473f9ae96c7e2ad6223691e2053edb58dda5
rhs0266 <dkflfkdeh@naver.com>
2016-05-04 20:40:20 +0900

    add Puzzle

---
+++ Assets/Scripts/DragGameObject.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using System.Collections;
+ 
+public class DragGameObject : MonoBehaviour
+{
+    private Rigidbody rb;
+    bool drag = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (drag)
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 50);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector3 difPosition = objPosition - transform.position;
+            Debug.Log("Curr : " + transform.position);
+            Debug.Log("Mouse : " + objPosition);
+            //transform.position = objPosition;
+            rb.AddForce(difPosition * 10);
+        }
+    }
+}
