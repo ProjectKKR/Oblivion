25ea4285db100041aa5a7c611cc00152ab2c7f61
rhs0266 <dkflfkdeh@naver.com>
2016-05-09 19:22:51 +0900

    Init branch

---
+++ Assets/Scripts/Inventory/ItemEquippingController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public ItemEquippingController : MonoBehavior{
+	public virtual void OnPointerDown(PointerEventData ped){
+
