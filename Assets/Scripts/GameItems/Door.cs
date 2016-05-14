0d644602cc477b555762a3cb9618a6846439a351
dn0530 <eodmsabc@gmail.com>
2016-05-14 20:51:15 +0900

    door open/close sound

--- Assets/Scripts/GameItems/Door.cs
+++ Assets/Scripts/GameItems/Door.cs
@@ -5,12 +5,27 @@ public class Door :  GameItems {
 	public float speed;
 	public GameObject axis;
 
+	private AudioSource openAudio;
+	private AudioSource closeAudio;
 	private Ray ray;
 	private RaycastHit hit;
 	private float openFlag=1.0f;
 
+	void Start () {
+		AudioSource[] sounds = GetComponents<AudioSource> ();
+		openAudio = sounds[0];
+		closeAudio = sounds[1];
+	}
+
 	protected override void Interaction () {
 		axis.transform.Rotate (new Vector3 (0, -80.0f * openFlag, 0));
 		openFlag = - openFlag;
+		if (openFlag == 1.0f) {
+			openAudio.Play ();
+		}
+		else {
+			AudioSource sound = GetComponent<AudioSource> ();
+			closeAudio.Play ();
+		}
 	}
 }
