3d0c544d55268f54305ce04cdf74132bc85a53d8
dn0530 <eodmsabc@gmail.com>
2016-05-15 21:20:50 +0900

    main menu modify for tizen store upload

---
+++ Assets/Scripts/Menu/LoadingMessage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadingMessage : MonoBehaviour {
+	private Text loadingMessage;
+	// Use this for initialization
+	void Start () {
+		loadingMessage = GetComponent<Text> ();
+		loadingMessage.text = "Now Loading";
+	}
+
+
+}
--- Assets/Scripts/Menu/StartController.cs
+++ Assets/Scripts/Menu/StartController.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class StartController : MonoBehaviour {
 	public GameObject SubStart;
+	public Text loadingMessage;
+	int state;
+
 	// Use this for initialization
 	void Start () {
 		SubStart.SetActive (false);
+		state = 0;
 	}
 	public void LoadSubStart(){
 		SubStart.SetActive (true);
-		//SceneManager.LoadScene ("Oblivion");
+		//InvokeRepeating ("textUpdate", 0.0f, 0.2f);
+		SceneManager.LoadScene ("Oblivion");
 	}
+	/*
+	public void textUpdate () {
+		loadingMessage.text = "Now Loading";
+		for (int i = 0; i < 4; i++) {
+			if (i < state)
+				loadingMessage.text = loadingMessage.text + ".";
+			else
+				loadingMessage.text = loadingMessage.text + " ";
+		}
+		print (loadingMessage.text);
+		state = (state + 1) % 4;
+	}
+	*/
 }
