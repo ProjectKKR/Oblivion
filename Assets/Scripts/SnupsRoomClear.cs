88c91bbb21807aff9f71da864716c8fb24ed3408
Ryu HoSeok <dkflfkdeh@naver.com>
2016-06-06 00:43:15 +0900

    fix ending menu

---
+++ Assets/Scripts/SnupsRoomClear.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Collections;
+
+public class SnupsRoomClear : MonoBehaviour {
+
+	public void SnupsClear(){
+		//TODO clear save information
+		EditorSceneManager.LoadScene("MainMenu");
+	}
+}
