2b063377256f2fc449463e6317fa736a21ea6816
Ryu HoSeok <dkflfkdeh@naver.com>
2016-06-05 23:43:15 +0900

    add smartphonezoom.cs

--- Assets/Scripts/Bacchus/SmartPhoneButton.cs
+++ Assets/Scripts/Bacchus/SmartPhoneButton.cs
@@ -3,11 +3,11 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class ComputerButton: MonoBehaviour{
-	private Computer com;
+public class SmartPhoneButton: MonoBehaviour{
+	private SmartPhone com;
 	public int number;
 	void Start(){
-		com = transform.parent.GetComponent<Computer>();
+		com = transform.parent.GetComponent<SmartPhone>();
 	}
 	public void NumberButton(){
 		com.Add (number);
--- Assets/Scripts/GameItems/SmartPhoneZoom.cs
+++ Assets/Scripts/GameItems/SmartPhoneZoom.cs
@@ -2,13 +2,28 @@
 using System.Collections;
 
 public class SmartPhoneZoom : GameItems{
+
+	public GameObject screen;
+	public GameObject charger;
+
 	void Start () {
 		zoomLocation = transform.position + new Vector3 (0.0f, 1.0f, 0.0f);
 		zoomRotation = Quaternion.Euler (new Vector3 (90, 180, 0));
 	}
 
-	protected override void Interaction () {
-		// TODO : check equipping charger
+	protected override bool EquippedItemCheck (GameItems equipped){
+		if (equipped == null) return false;
+		if (equipped.Equals (charger)) {
+			ChainOperation (0);
+			return true;
+		}
+		return false;
+	}
+
+	public override void ChainOperation (int caseNum) {
+		screen.SetActive (true);
+	}
+	protected override void Interaction(){
 	}
 
 }
