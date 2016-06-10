1c91178f3eedd438124a1fc04af1eaca3c8387ad
rhs0266 <dkflfkdeh@naver.com>
2016-06-10 16:39:58 +0900

    add diaries

--- Assets/Scripts/GameItems/Diary.cs
+++ Assets/Scripts/GameItems/Diary.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class Diary : GameItems {
-
+	public Vector3 position, rotation;
 	// Use this for initialization
 	void Start () {
-		zoomLocation = transform.position + new Vector3 (0.0f, 0.3f, 0.0f);
-		zoomRotation = Quaternion.Euler (new Vector3 (90, 41, 0));
+		zoomLocation = transform.position + position;
+		zoomRotation = Quaternion.Euler (rotation);
 	}
 	
 
